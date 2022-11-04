1aea4401d3b876c95f9e24766e00c7eb9ea8577e
Tatooine93 <nowakantoine93@gmail.com>
2022-11-04 11:41:27 +0100

    Finish Worskshop

--- socket-game/io/conf.js
+++ socket-game/io/conf.js
@@ -3,4 +3,34 @@ const socketio = require('socket.io');
 module.exports = function(server) {
   // io server
   const io = socketio(server);
+
+  const players = {};
+
+  io.on("connection", function (socket) {
+    
+    console.log("Player connected:" + socket.id);
+
+    players[socket.id] = {
+    x: 0,
+    y: 0,
+    size: 20,
+    speed: 5,
+    c: "#" + (((1 << 24) * Math.random()) | 0).toString(16),
+    };
+
+    socket.on("disconnect", function () {
+      delete players[socket.id];
+    });
+
+      socket.on('move left',  function() { players[socket.id].x -= players[socket.id].speed; });
+      socket.on('move up',    function() { players[socket.id].y -= players[socket.id].speed; });
+      socket.on('move right', function() { players[socket.id].x += players[socket.id].speed; });
+      socket.on('move down',  function() { players[socket.id].y += players[socket.id].speed; });
+  });
+
+  function update() {
+    io.volatile.emit('players list', Object.values(players))
+  };
+
+  setInterval(update, 1000/60);
 };
--- socket-game/public/game.js
+++ socket-game/public/game.js
@@ -1,1 +1,65 @@
+const socket = io();
 
+/* const player = {
+    x: 0,
+    y: 0,
+    size: 20,
+    speed: 5
+}; */
+
+let players = [];
+
+socket.on('players list', function (list) {
+    players = list;
+});
+
+const ctx = canvas.getContext("2d");
+
+/* function drawPlayer() {
+    const {x, y, size} = player;
+    ctx.beginPath();
+    ctx.rect(x, y, size, size);
+    ctx.fill();
+}; */
+
+function drawPlayers() {
+    players.forEach(function ({x, y, size, c}) {
+        ctx.beginPath();
+        ctx.rect(x, y, size, size);
+        ctx.fillStyle = c;
+        ctx.fill();
+    });
+};
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPlayers();
+    movePlayer();
+    requestAnimationFrame(update);
+}
+
+requestAnimationFrame(update);
+
+const keyboard = {};
+
+window.onkeydown = function(e) {
+    keyboard[e.key] = true;
+};
+
+window.onkeyup = function(e) {
+    delete keyboard[e.key];
+};
+
+/* function movePlayer() {
+    if (keyboard["ArrowLeft"]) player.x -= player.speed; // left
+    if (keyboard["ArrowUp"]) player.y -= player.speed; // up
+    if (keyboard["ArrowRight"]) player.x += player.speed; // right
+    if (keyboard["ArrowDown"]) player.y += player.speed; // down
+}; */
+
+function movePlayer() {
+    if (keyboard['ArrowLeft']) socket.emit('move left');
+    if (keyboard['ArrowUp']) socket.emit('move up');
+    if (keyboard['ArrowRight']) socket.emit('move right');
+    if (keyboard['ArrowDown']) socket.emit('move down');
+};
